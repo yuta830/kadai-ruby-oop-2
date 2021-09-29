3fafd4a44fb3ce8024b8486b39cd419a1b70c874
yuta.kato <yyyutaa7@gmail.com>
2021-09-29 13:45:58 +0000

    oop-2

--- mentor.rb
+++ mentor.rb
@@ -4,28 +4,20 @@ class Mentor
     def initialize(name)
         self.name = name
     end
-end
-
-class Kirameki < Mentor
-    def initialize(name)
-        self.name = name
-    end
     def job(mentor)
         puts "#{self.name}です。私は現役のITプロフェッショナルです"
     end
 end
 
-class Akaide < Mentor
-    def initialize(name)
-        self.name = name
-    end
+class RailsMentor <Mentor
+    attr_accessor :name
     def job(mentor)
         puts "#{self.name}です。私はRubyとRailsでWebアプリケーションを作ります。"
     end
 end
 
-kirameki = Kirameki.new("煌木")
-akaide = Akaide.new("赤出")
+kirameki = Mentor.new("煌木")
+akaide = RailsMentor.new("赤出")
 
 kirameki.job("mentor")
 akaide.job("mentor")
